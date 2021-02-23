65864b331a626534151921691c4fc0452815c154
viololahe <65051338+viololahe@users.noreply.github.com>
2021-02-23 03:39:10 -0500

    Update pokemon.ino

--- pokemon.ino
+++ pokemon.ino
@@ -233,7 +233,7 @@ bool select1 = false;
 
 void setup() {
   Serial.begin(115200);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
